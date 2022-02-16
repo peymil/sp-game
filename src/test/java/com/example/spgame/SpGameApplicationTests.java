dde4822498ca9aa9654f2aabebec091a10561153
Emre Badem <emrebadem_@hotmail.com>
2022-02-16 23:50:15 +0300

    Initialized springboot

---
+++ src/test/java/com/example/spgame/SpGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
